66503c7e034a24c247290e93246735fa48a22de9
michaelkeates <mail@michaelkeates.co.uk>
2023-09-18 16:32:23 +0100

    Update flake.nix

--- main/flake.nix
+++ main/flake.nix
@@ -49,16 +49,16 @@
       };
     in
     {
-      #templates = {
-      #  starter = {
-      #    path = ./templates/starter;
-      #    description = "Starter configuration";
-      #  };
+      templates = {
+        main = {
+          path = ./templates/main;
+          description = "Starter configuration";
+        };
       #  starterWithSecrets = {
       #    path = ./templates/starterWithSecrets;
       #    description = "Starter configuration with secrets";
       #  };
-      #};
+      };
 
       devShells = forAllSystems devShell;
 
