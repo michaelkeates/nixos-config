f9b9c6db3966c7c2c5058eac218633277a598166
michaelkeates <mail@michaelkeates.co.uk>
2023-09-18 16:08:06 +0100

    nextcloud

---
+++ main/darwin/casks.nix
@@ -0,0 +1,28 @@
+{}:
+
+[
+  # Development Tools
+  "homebrew/cask/docker"
+  "postico"
+  "visual-studio-code"
+
+  # Communication Tools
+  "discord"
+  "telegram"
+  "zoom"
+
+  # Utility Tools
+  "appcleaner"
+  "syncthing"
+  "raycast"
+  "keepingyouawake"
+  "warp"
+
+  # Entertainment Tools
+  "steam"
+
+  # Productivity Tools
+
+  # Browsers
+  "min"
+]
