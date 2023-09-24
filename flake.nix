5a59203357a8a2666f6bf115267a80be1bcc4760
michaelkeates <mail@michaelkeates.co.uk>
2023-09-24 15:14:24 +0100

    update

--- flake.nix
+++ flake.nix
@@ -71,8 +71,8 @@
     in
     {
       templates = {
-        main = {
-          path = ./templates/main;
+        starter = {
+          path = ./templates/starter;
           description = "Starter configuration with secrets";
         };
       };
