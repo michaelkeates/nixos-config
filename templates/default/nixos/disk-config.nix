3a299352e117d9f71da8b637b8eac803811ef15c
michaelkeates <mail@michaelkeates.co.uk>
2023-10-02 15:03:49 +0100

    disko

--- templates/default/nixos/disk-config.nix
+++ templates/default/nixos/disk-config.nix
@@ -1,33 +1,29 @@
-{ ... }: {
-  disko.devices = {
-    disk = {
-      nvda = {  # Change "nvme0n1" to "nvda"
-        device = "/dev/nvda";  # Change "/dev/nvme0n1" to "/dev/nvda"
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              type = "EF00";
-              size = "600M";  # Change size to 600M
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            root = {
-              start = "600M";  # Start at 600M
-              size = "100%";   # Takes the remaining space
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            };
-          };
-        };
-      };
-    };
-  };
+{ disks ? [ "/dev/vda" ], ... }: {
+ disk = {
+  vda = {
+   device = builtins.elemAt disks 0;
+   type = "disk";
+   content = {
+    type = "gpt";
+    partitions = {
+     ESP = {
+      size = "100M";
+      content = {
+       type = "filesystem";
+       format = "vfat";
+       mountpoint = "/boot";
+      };
+     };
+     root = {
+      size = "100%";
+      content = {
+       type = "filesystem";
+       format = "ext4";
+       mountpoint = "/";
+      };
+     };
+    };
+   };
+  };
+ };
 }
