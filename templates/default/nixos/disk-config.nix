2b2b7bf210376ee6748d93db7deee7ab5cb787a0
michaelkeates <mail@michaelkeates.co.uk>
2023-10-02 13:47:59 +0100

    Update disk-config.nix

--- templates/default/nixos/disk-config.nix
+++ templates/default/nixos/disk-config.nix
@@ -1,35 +1,33 @@
 { ... }: {
-# This formats the disk with the ext4 filesystem
-# Other examples found here: https://github.com/nix-community/disko/tree/master/example
-#  disko.devices = {
-#    disk = {
-#      vdb = {
-#        # device = "/dev/disk/by-id/some-disk-id";
-#        device = "/dev/nvme0n1";
-#        type = "disk";
-#        content = {
-#          type = "gpt";
-#          partitions = {
-#            ESP = {
-#              type = "EF00";
-#              size = "100M";
-#              content = {
-#                type = "filesystem";
-#                format = "vfat";
-#                mountpoint = "/boot";
-#              };
-#            };
-#            root = {
-#              size = "100%";
-#              content = {
-#                type = "filesystem";
-#                format = "ext4";
-#                mountpoint = "/";
-#              };
-#            };
-#          };
-#        };
-#      };
-#    };
-#  };
-#}
+  disko.devices = {
+    disk = {
+      nvda = {  # Change "nvme0n1" to "nvda"
+        device = "/dev/nvda";  # Change "/dev/nvme0n1" to "/dev/nvda"
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "600M";  # Change size to 600M
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              start = "600M";  # Start at 600M
+              size = "100%";   # Takes the remaining space
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
