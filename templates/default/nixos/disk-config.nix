b96163a1fb34af2894cac4ca2da0e58c5bcb57d1
michaelkeates <mail@michaelkeates.co.uk>
2023-10-02 15:59:23 +0100

    disko

---
+++ templates/default/nixos/disk-config.nix
@@ -0,0 +1,27 @@
+{
+  disko.devices = {
+    disk = {
+      vda = {
+        device = "/dev/vda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF00"; # for grub MBR
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
