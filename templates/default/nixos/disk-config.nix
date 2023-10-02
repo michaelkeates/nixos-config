b8e79cffd06173039113f76789d3bff97b257818
michaelkeates <mail@michaelkeates.co.uk>
2023-10-02 20:03:50 +0100

    Update disk-config.nix

--- templates/default/nixos/disk-config.nix
+++ templates/default/nixos/disk-config.nix
@@ -1,38 +1,31 @@
-{ ... }: {
-# This formats the disk with the ext4 filesystem
-# Other examples found here: https://github.com/nix-community/disko/tree/master/example
-  disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        device = "/dev/vda";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02"; # for grub MBR
-            };
-            ESP = {
-              size = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            };
-          };
-        };
-      };
-    };
+{ disks ? [ "/dev/vda" ], ... }: {
+ disko.devices = {
+  disk = {
+   vda = {
+    device = builtins.elemAt disks 0;
+    type = "disk";
+    content = {
+     type = "gpt";
+     partitions = {
+      ESP = {
+       size = "500M";
+       content = {
+        type = "filesystem";
+        format = "vfat";
+        mountpoint = "/boot";
+       };
+      };
+      root = {
+       size = "100%";
+       content = {
+        type = "filesystem";
+        format = "ext4";
+        mountpoint = "/";
+       };
+      };
+     };
+    };
+   };
   };
+ };
 }
