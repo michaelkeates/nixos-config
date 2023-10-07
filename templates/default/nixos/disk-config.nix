b5bf676ed015a0287cd97e526812d1bbd56b2b9b
michaelkeates <mail@michaelkeates.co.uk>
2023-10-07 17:26:00 +0100

    Update disk-config.nix

--- templates/default/nixos/disk-config.nix
+++ templates/default/nixos/disk-config.nix
@@ -1,31 +1,35 @@
-{ disks ? [ "/dev/vda" ], ... }: {
- disko.devices = {
-  disk = {
-   vda = {
-    device = builtins.elemAt disks 0;
-    type = "disk";
-    content = {
-     type = "gpt";
-     partitions = {
-      ESP = {
-       size = "500M";
-       content = {
-        type = "filesystem";
-        format = "vfat";
-        mountpoint = "/boot";
-       };
-      };
-      root = {
-       size = "100%";
-       content = {
-        type = "filesystem";
-        format = "ext4";
-        mountpoint = "/";
-       };
-      };
-     };
-    };
-   };
+{ ... }: {
+# This formats the disk with the ext4 filesystem
+# Other examples found here: https://github.com/nix-community/disko/tree/master/example
+    disko.devices = {
+        disk = {
+            vda = {
+                #device = "/dev/disk/by-id/some-disk-id";
+                device = "/dev/vda";
+                type = "disk";
+                content = {
+                    type = "gpt";
+                    partitions = {
+                        ESP = {
+                            type = "EF00";
+              size = "100M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
   };
- };
 }
