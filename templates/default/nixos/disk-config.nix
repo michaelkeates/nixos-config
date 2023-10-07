e4d3c41af752204fc25463cbf192f0cac08dd4a2
michaelkeates <mail@michaelkeates.co.uk>
2023-10-07 19:26:26 +0100

    Update disk-config.nix

--- templates/default/nixos/disk-config.nix
+++ templates/default/nixos/disk-config.nix
@@ -1,16 +1,17 @@
 { ... }: {
 # This formats the disk with the ext4 filesystem
 # Other examples found here: https://github.com/nix-community/disko/tree/master/example
-  disko.devices = {
-    disk = {
-      vda = {
-        device = "/dev/disk/by-id/virtio-vdisk1";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              type = "EF00";
+    disko.devices = {
+        disk = {
+            vda = {
+                #device = "/dev/disk/by-id/some-disk-id";
+                device = "/dev/vda";
+                type = "disk";
+                content = {
+                    type = "gpt";
+                    partitions = {
+                        ESP = {
+                            type = "EF00";
               size = "100M";
               content = {
                 type = "filesystem";
