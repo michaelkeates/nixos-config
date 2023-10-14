89ec689510306fb1206494ec66da82b4f500ee03
michaelkeates <mail@michaelkeates.co.uk>
2023-10-14 20:15:20 +0100

    Update default.nix

--- templates/default/nixos/default.nix
+++ templates/default/nixos/default.nix
@@ -16,6 +16,7 @@ let user = "mike";
   #boot.loader.systemd-boot.configurationLimit = 42;
   boot.loader.efi.canTouchEfiVariables = true;
   # boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  # make sure to include modules, these are needed for VMs to work
   boot.initrd.availableKernelModules = [ "virtio_net" "virtio_pci" "virtio_mmio" "virtio_blk" "virtio_scsi" "9p" "9pnet_virtio" ];
   boot.initrd.kernelModules = [ "virtio_balloon" "virtio_console" "virtio_rng" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
