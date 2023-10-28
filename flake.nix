86f8574279453b3e3ee7538bde22a2cfb0429798
michaelkeates <mail@michaelkeates.co.uk>
2023-10-28 13:40:50 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     secrets = {
-      url = "git+ssh://git@github.com/michaelkeates/nix-secrets.git";
+      url = "git+ssh://git@github.com/michaelkeates/main";
       flake = false;
     };
   };
