a0b0f1caabb9ee479e5786795408b9fdbc4d06e7
michaelkeates <mail@michaelkeates.co.uk>
2023-10-29 22:53:38 +0000

    Update secrets.nix

--- templates/default/nixos/secrets.nix
+++ templates/default/nixos/secrets.nix
@@ -7,10 +7,10 @@ let user = "mike"; in
     "/home/${user}/.ssh/id_ed25519"
   ];
 
-  age.secrets."bitwarden-masterpassword" = {
+  age.secrets."secret" = {
     symlink = false;
     path = "/home/${user}/.config/Bitwarden/masterpassword";
-    file =  "${secrets}/bitwarden-masterpassword.age";
+    file =  "${secrets}/secret.age";
     mode = "600";
     owner = "${user}";
     group = "users";
