7364a101434c6d99d30cf216079079e09d173ba9
michaelkeates <mail@michaelkeates.co.uk>
2023-11-01 20:58:37 +0000

    update

--- templates/default/shared/default.nix
+++ templates/default/shared/default.nix
@@ -1,35 +1,40 @@
 { config, pkgs, ... }:
 
 let
-  fontDir = ./fonts;
+  installFonts = pkgs.writeText "install-fonts.sh" ''
+    mkdir -p $out/share/fonts
 
-installFonts = pkgs.writeText "install-fonts.sh" ''
-  mkdir -p $out/share/fonts
+    if [ "$(uname -s)" = "Darwin" ]; then
+      fontDest=$out/Library/Fonts
+    else
+      fontDest=$out/share/fonts
+    fi
 
-  if [ "$(uname -s)" = "Darwin" ]; then
-    fontDest=$out/Library/Fonts
-  else
-    fontDest=$out/share/fonts
-  fi
+    # Download fonts from the hidden GitHub repo
+    git clone git+ssh://git@github.com/michaelkeates/personal-fonts.git
+    unzip fonts.zip -d $out/share/fonts
 
-  for fontFile in ${fontDir}/*; do
-    fontType=$(file -b --mime-type "$fontFile")
-    case "$fontType" in
-      application/x-font-ttf)
-        cp "$fontFile" $fontDest/TTF/
-        ;;
-      application/x-font-opentype)
-        cp "$fontFile" $fontDest/OTF/
-        ;;
-      application/x-font-type1)
-        cp "$fontFile" $fontDest/Type1/
-        ;;
-      *)
-        cp "$fontFile" $fontDest/misc/
-        ;;
-    esac
-  done
-'';
+    # Organize fonts (similar to previous script)
+    for fontFile in $out/share/fonts/*; do
+      fontType=$(file -b --mime-type "$fontFile")
+      case "$fontType" in
+        application/x-font-ttf)
+          mv "$fontFile" $fontDest/TTF/
+          ;;
+        application/x-font-opentype)
+          mv "$fontFile" $fontDest/OTF/
+          ;;
+        application/x-font-type1)
+          mv "$fontFile" $fontDest/Type1/
+          ;;
+        *)
+          mv "$fontFile" $fontDest/misc/
+          ;;
+      esac
+    done
+
+    rm -rf fonts.zip $out/share/fonts  # Clean up downloaded fonts
+  '';
 
   emacsOverlaySha256 = "06413w510jmld20i4lik9b36cfafm501864yq8k4vxl5r4hn0j0h";
 in
@@ -57,8 +62,8 @@ in
 
     fonts = pkgs.stdenv.mkDerivation {
       name = "my-fonts";
-      src = null;  # No source needed, as we're using local files
-      buildInputs = [ pkgs.bash ];  # Ensure bash is available
+      src = null;  # No source needed, as we're downloading from a repo
+      buildInputs = [ pkgs.bash unzip ];  # Ensure bash and unzip are available
       installPhase = installFonts;
     };
   };
