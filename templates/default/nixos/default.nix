c0cb9cd6dbb4ecbde1fa231033d5bd74719239c7
michaelkeates <103878560+michaelkeates@users.noreply.github.com>
2023-11-05 19:00:14 +0000

    Update default.nix

--- templates/default/nixos/default.nix
+++ templates/default/nixos/default.nix
@@ -68,13 +68,13 @@ in
 
   # LightDM Display Manager
   services.xserver.displayManager.defaultSession = "none+bspwm";
-  services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
   # services.xserver.displayManager.gnome.enable = true;
-  # services.xserver.displayManager.lightdm = {
-  #  enable = true;
-  #  greeters.slick.enable = true;
-  #  background = ./config/login-wallpaper.jpg;
-  #};
+  services.xserver.displayManager.lightdm = {
+   enable = true;
+   greeters.slick.enable = true;
+   background = ./config/login-wallpaper.jpg;
+  };
 
   # Tiling window manager
   services.xserver.windowManager.bspwm = {
