fd9c904a39e0cc46b07388007bb710a58461ae44
michaelkeates <mail@michaelkeates.co.uk>
2023-11-11 16:21:28 +0000

    Update packages.nix

--- templates/default/nixos/packages.nix
+++ templates/default/nixos/packages.nix
@@ -3,7 +3,7 @@
 with pkgs;
 let
   shared-packages = import ../shared/packages.nix { inherit pkgs; };
-  thorium = import ./thorium.nix { inherit pkgs fetchurl lib fontconfig stdenv xorg liberation_ttf gtk3; };
+  thorium = import ./thorium.nix { inherit pkgs fetchurl lib fontconfig stdenv xorg liberation_ttf gtk3 nss; };
 in
 shared-packages ++ [
 
