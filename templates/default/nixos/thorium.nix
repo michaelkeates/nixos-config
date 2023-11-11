4604dbbdc2123fdcfb29da985bbcfaaec53b2e36
michaelkeates <mail@michaelkeates.co.uk>
2023-11-11 18:24:10 +0000

    Update thorium.nix

--- templates/default/nixos/thorium.nix
+++ templates/default/nixos/thorium.nix
@@ -1,6 +1,6 @@
 # thorium.nix
 
-{ stdenv, fetchurl, autoPatchelfHook, dpkg, wrapGAppsHook, qt6.wrapQtAppsHook, lib }:
+{ stdenv, fetchurl, autoPatchelfHook, dpkg, wrapGAppsHook, qt6.wrapQtAppsHook, lib, qt6 }:
 
 stdenv.mkDerivation rec {
   pname = "thorium-browser";
