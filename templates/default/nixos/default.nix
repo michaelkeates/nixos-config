ac1ace0be4da2dcee147ee50d9d596ea3bf2ab7c
michaelkeates <mail@michaelkeates.co.uk>
2023-11-12 12:15:11 +0000

    Update default.nix

--- templates/default/nixos/default.nix
+++ templates/default/nixos/default.nix
@@ -78,7 +78,7 @@ in
    #greeters.slick.enable = true;
    webkitTheme = fetchTarball {
      url = "https://github.com/Litarvan/lightdm-webkit-theme-litarvan/releases/download/v3.2.0/lightdm-webkit-theme-litarvan-3.2.0.tar.gz";
-     sha256 = "1TfNhwM8xVAEWQa5bBdv8WlmE3Q9AkpworEDDGsLbR4I=";
+     sha256 = "1TfNhwM8xVAEWQa5bBdv8WlmE3Q9AkpworEDDGsLbR4I";
     };
    background = ./config/login-wallpaper.jpg;
   };
