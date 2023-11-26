574020917aeb6f944d97a11addd0bf090fd54e9a
michaelkeates <mail@michaelkeates.co.uk>
2023-11-26 14:22:35 +0000

    Update default.nix

--- templates/default/nixos/default.nix
+++ templates/default/nixos/default.nix
@@ -104,9 +104,9 @@ in
   services.xserver.displayManager.sddm = {
     enable = true;
     theme = "${(pkgs.fetchFromGitHub {
-    owner = "MarianArlt";
-    repo = "kde-plasma-chili";
-    rev = "a371123959676f608f01421398f7400a2f01ae06";
+    owner = "3ximus";
+    repo = "aerial-sddm-theme";
+    rev = "2d0b631e73f2a6479fb72655de65c74054ac2846";
     sha256 = "17pkxpk4lfgm14yfwg6rw6zrkdpxilzv90s48s2hsicgl3vmyr3x";
   })}";
 
