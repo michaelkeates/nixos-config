5a0a17e6170c3129b1638a107ac59f3212c5ff62
michaelkeates <mail@michaelkeates.co.uk>
2023-11-27 20:22:53 +0000

    Update default.nix

--- templates/default/nixos/default.nix
+++ templates/default/nixos/default.nix
@@ -24,9 +24,9 @@ in
   # boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   # make sure to include modules, these are needed for VMs to work
   boot.initrd.availableKernelModules = [ "virtio_net" "virtio_pci" "virtio_mmio" "virtio_blk" "virtio_scsi" "9p" "9pnet_virtio" ];
-  boot.initrd.kernelModules = [ "virtio_balloon" "virtio_console" "virtio_rng" "8761bu_fw" ];
+  boot.initrd.kernelModules = [ "virtio_balloon" "virtio_console" "virtio_rng" "8761bu" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.extraModulePackages = [ config.boot.kernelPackages.rtl8761bu_fw ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.rtl8761bu ];
 
   # Enable bluetooth
   services.blueman.enable = true;
