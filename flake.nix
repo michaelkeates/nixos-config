cd8d72788f4e9b065c1ce838c9c04a59c4f0c637
Michael Keates <mail@michaelkeates.co.uk>
2023-12-17 15:14:09 +0000

    fix

--- flake.nix
+++ flake.nix
@@ -1,3 +1,4 @@
+
 {
   description = "Mike's Configuration for NixOS and MacOS";
 
@@ -33,102 +34,96 @@
       flake = false;
     };
   };
-
   outputs = { self, darwin, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle, home-manager, nixpkgs, disko, agenix, secrets } @inputs:
-
-  let
-    user = "mike";
-    linuxSystems = [ "x86_64-linux" "aarch64-linux" ];
-    darwinSystems = [ "aarch64-darwin" ];
-    forAllLinuxSystems = f: nixpkgs.lib.genAttrs linuxSystems (system: f system);
-    forAllDarwinSystems = f: nixpkgs.lib.genAttrs darwinSystems (system: f system);
-    forAllSystems = f: nixpkgs.lib.genAttrs (linuxSystems ++ darwinSystems) (system: f system);
-
-    devShell = system: let pkgs = nixpkgs.legacyPackages.${system}; in {
-      default = with pkgs; mkShell {
-        nativeBuildInputs = with pkgs; [ bashInteractive git age age-plugin-yubikey ];
-        shellHook = with pkgs; ''
-          export EDITOR=vim
-        '';
+    let
+      user = "mike";
+      linuxSystems = [ "x86_64-linux" "aarch64-linux" ];
+      darwinSystems = [ "aarch64-darwin" ];
+      forAllLinuxSystems = f: nixpkgs.lib.genAttrs linuxSystems (system: f system);
+      forAllDarwinSystems = f: nixpkgs.lib.genAttrs darwinSystems (system: f system);
+      forAllSystems = f: nixpkgs.lib.genAttrs (linuxSystems ++ darwinSystems) (system: f system);
+      devShell = system: let pkgs = nixpkgs.legacyPackages.${system}; in {
+        default = with pkgs; mkShell {
+          nativeBuildInputs = with pkgs; [ bashInteractive git age age-plugin-yubikey ];
+          shellHook = with pkgs; ''
+            export EDITOR=vim
+          '';
+        };
       };
-    };
-
-    mkApp = scriptName: template: system: {
-      type = "app";
-      program = "${(nixpkgs.legacyPackages.${system}.writeScriptBin scriptName ''
-        #!/usr/bin/env bash
-        PATH=${nixpkgs.legacyPackages.${system}.git}/bin:$PATH
-        echo "Running ${scriptName} for ${system} with template ${template}"
-        exec ${self}/apps/${system}/${template}/${scriptName}
-      '')}/bin/${scriptName}";
-    };
-
-    mkLinuxApps = system: template: {
-      "install" = mkApp "install" template system;
-      "rebuild" = mkApp "rebuild" template system;
-      "copyKeys" = mkApp "copyKeys" template system;
-      "createKeys" = mkApp "createKeys" template system;
-      "checkKeys" = mkApp "checkKeys" template system;
-      "install_test" = mkApp "install" "docker" system;
-    };
-
-    mkDarwinApps = system: {
-      "copyKeys" = mkApp "copyKeys" system;
-      "createKeys" = mkApp "createKeys" system;
-      "checkKeys" = mkApp "checkKeys" system;
-      "install" = mkApp "install" system;
-      "rebuild" = mkApp "rebuild" system;
-    };
-  in
-  {
-    templates = {
-      default = {
-        path = ./templates/default;
-        description = "Starter configuration";
+      mkApp = scriptName: system: {
+        type = "app";
+        program = "${(nixpkgs.legacyPackages.${system}.writeScriptBin scriptName ''
+          #!/usr/bin/env bash
+          PATH=${nixpkgs.legacyPackages.${system}.git}/bin:$PATH
+          echo "Running ${scriptName} for ${system}"
+          exec ${self}/apps/${system}/${scriptName}
+        '')}/bin/${scriptName}";
       };
-      docker = {
-        path = ./templates/docker;
-        description = "Docker Server";
+      mkLinuxApps = system: {
+        "install" = mkApp "install" system;
+        "rebuild" = mkApp "rebuild" system;
+        "copyKeys" = mkApp "copyKeys" system;
+        "createKeys" = mkApp "createKeys" system;
+        "checkKeys" = mkApp "checkKeys" system;
+        "install_test" = mkApp "docker/install" system;
       };
-    };
-    devShells = forAllSystems devShell;
-    apps = nixpkgs.lib.genAttrs linuxSystems mkLinuxApps // nixpkgs.lib.genAttrs darwinSystems mkDarwinApps;
-    darwinConfigurations = let user = "mike"; in {
-      "Mikes-MBA" = darwin.lib.darwinSystem {
-        system = "aarch64-darwin";
+      mkDarwinApps = system: {
+        "copyKeys" = mkApp "copyKeys" system;
+        "createKeys" = mkApp "createKeys" system;
+        "checkKeys" = mkApp "checkKeys" system;
+        "install" = mkApp "install" system;
+        "rebuild" = mkApp "rebuild" system;
+      };
+    in
+    {
+      templates = {
+        default = {
+          path = ./templates/default;
+          description = "Starter configuration";
+        };
+        docker = {
+          path = ./templates/docker;
+          description = "Docker Server";
+        };
+      };
+      devShells = forAllSystems devShell;
+      apps = nixpkgs.lib.genAttrs linuxSystems mkLinuxApps // nixpkgs.lib.genAttrs darwinSystems mkDarwinApps;
+      darwinConfigurations = let user = "mike"; in {
+        "Mikes-MBA" = darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          specialArgs = inputs;
+          modules = [
+            home-manager.darwinModules.home-manager
+            nix-homebrew.darwinModules.nix-homebrew
+            {
+              nix-homebrew = {
+                enable = true;
+                user = "${user}";
+                taps = {
+                  "homebrew/homebrew-core" = homebrew-core;
+                  "homebrew/homebrew-cask" = homebrew-cask;
+                  "homebrew/bundle" = homebrew-bundle;
+                };
+                mutableTaps = false;
+                autoMigrate = true;
+              };
+            }
+            ./darwin
+          ];
+        };
+      };
+      nixosConfigurations = nixpkgs.lib.genAttrs linuxSystems (system: nixpkgs.lib.nixosSystem {
+        system = system;
         specialArgs = inputs;
         modules = [
-          home-manager.darwinModules.home-manager
-          nix-homebrew.darwinModules.nix-homebrew
-          {
-            nix-homebrew = {
-              enable = true;
-              user = "${user}";
-              taps = {
-                "homebrew/homebrew-core" = homebrew-core;
-                "homebrew/homebrew-cask" = homebrew-cask;
-                "homebrew/bundle" = homebrew-bundle;
-              };
-              mutableTaps = false;
-              autoMigrate = true;
-            };
+          disko.nixosModules.disko
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.${user} = import ./nixos/home-manager.nix;
           }
-          ./darwin
+          ./nixos
         ];
-      };
-    };
-    nixosConfigurations = nixpkgs.lib.genAttrs linuxSystems (system: nixpkgs.lib.nixosSystem {
-      system = system;
-      specialArgs = inputs;
-      modules = [
-        disko.nixosModules.disko
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.${user} = import ./nixos/home-manager.nix;
-        }
-        ./nixos
-      ];
-    });
+     });
   };
 }
