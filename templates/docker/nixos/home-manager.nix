2f34f9dd85ed94102e03e57e7b5d867daeb02307
Michael Keates <mail@michaelkeates.co.uk>
2024-01-07 12:22:21 +0000

    Update home-manager.nix

--- templates/docker/nixos/home-manager.nix
+++ templates/docker/nixos/home-manager.nix
@@ -15,10 +15,6 @@ let name = "Michael Keates";
     };
     extraConfig = {
       init.defaultBranch = "main";
-      core = { 
-	    editor = "vim";
-        autocrlf = "input";
-      };
       commit.gpgsign = true;
       pull.rebase = true;
       rebase.autoStash = true;
