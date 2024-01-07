93ece0a7f33b63aa5fb8b8c8b4eb35daa0e1b6e0
Michael Keates <mail@michaelkeates.co.uk>
2024-01-07 14:32:59 +0000

    Update home-manager.nix

--- templates/docker/nixos/home-manager.nix
+++ templates/docker/nixos/home-manager.nix
@@ -10,24 +10,23 @@ let
   xdg_configHome  = "/home/${homeuser}/.config";
 in
 {
-  home-manager.users.${homeuser} = {
+home-manager.users.${homeuser} = {
     enableNixpkgsReleaseCheck = false;
     username = "${homeuser}";
     homeDirectory = "/home/${homeuser}";
     packages = pkgs.callPackage ./packages.nix {};
     stateVersion = "23.11";
+    git = {
+      enable = true;
+      ignores = [ "*.swp" ];
+      userName = name;
+      userEmail = email;
+      lfs = {
+        enable = true;
+      };
+    };
   };
 
   # Auto mount devices
   services.udiskie.enable = true;
-
-  git = {
-    enable = true;
-    ignores = [ "*.swp" ];
-    userName = name;
-    userEmail = email;
-    lfs = {
-      enable = true;
-    };
-  };
 }
