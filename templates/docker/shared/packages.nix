e0f0b466e70c77b9361250662eb6f7e99b9e1991
michaelkeates <103878560+michaelkeates@users.noreply.github.com>
2024-01-21 12:52:30 +0000

    Update packages.nix

--- templates/docker/shared/packages.nix
+++ templates/docker/shared/packages.nix
@@ -4,15 +4,5 @@ with pkgs; [
   # General packages for development and system management
   docker
   docker-compose
-  bash-completion
-  bat
-  coreutils
-  difftastic
-  du-dust
-  gcc
-  git-filter-repo
-  killall
   openssh
-  pandoc
-  wget
 ]
