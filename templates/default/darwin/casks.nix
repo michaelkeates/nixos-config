cef11e174e08e513385de2c9eeaf3ad12794c2f3
Michael Keates <mail@michaelkeates.co.uk>
2024-04-27 11:41:42 +0100

    Update casks.nix

--- templates/default/darwin/casks.nix
+++ templates/default/darwin/casks.nix
@@ -2,22 +2,17 @@
 
 [
   # Development Tools
-  #"homebrew/cask/docker"
   "homebrew/cask-versions/mono-mdk-for-visual-studio"
   "visual-studio"
   "android-studio"
 
   # Communication Tools
-  #"discord"
   "telegram"
-  #"zoom"
   "mailspring"
 
   # Utility Tools
   "appcleaner"
-  #"syncthing"
   "keepingyouawake"
-  # "warp"
   "bitwarden"
   "nextcloud"
   "crystalfetch"
